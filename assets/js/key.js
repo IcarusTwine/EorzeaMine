bce609a58e1903e1df090e6886f55f6feef5c553
Icarus <36118925+IcarusTwine@users.noreply.github.com>
2019-09-15 09:21:33 +0100

    final parse

---
+++ assets/js/key.js
@@ -0,0 +1,347 @@
+ 
+    function key() {
+      var x = document.getElementById("key");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+    function allmarkers() {
+      var x = document.getElementById("allmarkers");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+    function model() {
+      var x = document.getElementById("model");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function light() {
+      var x = document.getElementById("light");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function vfx() {
+      var x = document.getElementById("vfx");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function positionmarker() {
+      var x = document.getElementById("positionmarker");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function gimmick() {
+      var x = document.getElementById("gimmick");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function sound() {
+      var x = document.getElementById("sound");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function eventnpc() {
+      var x = document.getElementById("eventnpc");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function battlenpc() {
+      var x = document.getElementById("battlenpc");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function aetheryte() {
+      var x = document.getElementById("aetheryte");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function envspace() {
+      var x = document.getElementById("envspace");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function gathering() {
+      var x = document.getElementById("gathering");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function treasure() {
+      var x = document.getElementById("treasure");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function weapon() {
+      var x = document.getElementById("weapon");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function poprange() {
+      var x = document.getElementById("poprange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function exitrange() {
+      var x = document.getElementById("exitrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function maprange() {
+      var x = document.getElementById("maprange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function navimeshrange() {
+      var x = document.getElementById("navimeshrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function eventobject() {
+      var x = document.getElementById("eventobject");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function envlocation() {
+      var x = document.getElementById("envlocation");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function eventrange() {
+      var x = document.getElementById("eventrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function questmarker() {
+      var x = document.getElementById("questmarker");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function collisionbox() {
+      var x = document.getElementById("collisionbox");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function doorrange() {
+      var x = document.getElementById("doorrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function linevfx() {
+      var x = document.getElementById("linevfx");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function clientpath() {
+      var x = document.getElementById("clientpath");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function serverpath() {
+      var x = document.getElementById("serverpath");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function gimmickrange() {
+      var x = document.getElementById("gimmickrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function targetmarker() {
+      var x = document.getElementById("targetmarker");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function chairmarker() {
+      var x = document.getElementById("chairmarker");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function clickablerange() {
+      var x = document.getElementById("clickablerange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function prefetchrange() {
+      var x = document.getElementById("prefetchrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function faterange() {
+      var x = document.getElementById("faterange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
+
+
+    function spherecastrange() {
+      var x = document.getElementById("spherecastrange");
+      if (x.style.display === "none") {
+x.style.display = "block";
+      } else {
+x.style.display = "none";
+      }
+    }
